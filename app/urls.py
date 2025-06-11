f921add8fea503db9ce12e5fa23f30a3342b1bbb
Gabriel_Nebraska <gabrielviniciuscorrea204@gmail.com>
2025-06-11 09:55:30 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,11 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
+
 from rest_framework.routers import DefaultRouter
 
 from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet, AutorViewSet, LivroViewSet
@@ -36,4 +41,9 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),  
+
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
